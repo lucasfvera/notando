ce5d93a1913964fee4fc30b7e0fbd404106b09e6
Lucas <71686993+lucasfvera@users.noreply.github.com>
2021-02-28 16:43:31 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -109,7 +109,6 @@ function App() {
           <h3 className="subheader-text primary-text-color">
             Crea notas y mantené al día tus listas
           </h3>
-          <h1>Chau</h1>
         </div>
 
         <div className="bodyContainer">
