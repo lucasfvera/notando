80633ade60357bdd585ddc90dc5e38278b393aec
lucasfvera <lukaz_131@hotmail.com>
2021-11-13 20:24:04 -0300

    Add popup to send user feedback

--- src/index.js
+++ src/index.js
@@ -15,6 +15,12 @@ Sentry.init({
 	// We recommend adjusting this value in production
 	tracesSampleRate: 1.0,
 	// debug: true,
+	beforeSend(event, hint) {
+      // Check if it is an exception, and if so, show the report dialog
+      if (event.exception) {
+        Sentry.showReportDialog({ eventId: event.event_id });
+      }
+      return event;
     },
   });
 
