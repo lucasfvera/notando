13da4584607c24f33d9ff969f8867bf3a74f440d
lucasfvera <lukaz_131@hotmail.com>
2021-11-13 20:24:32 -0300

    Add failing btn to test issue log

--- src/components/card-list-component/cardList.jsx
+++ src/components/card-list-component/cardList.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import {Card, EmptyCard} from '../index'
+import * as Sentry from '@sentry/react'
 import './cardList.css'
 
 const CardList = ({notes, removeNote, editNote}) => {
     return (
         <ul className='cardListContainer'>
+            <button onClick={() => {
+                try{
+                    console.log(process.env)
+                    unexistentfn()
+                } catch (e) {
+                    console.error(`onClick fn in CardList component failed with error: ${e}`)
+                    Sentry.captureException(e)
+                }
+            }}>Break the world</button>
             {
-                !notes.length?
-                    <EmptyCard />
-                :
-
-                    notes.map(e => (
-                        // console.log('mapeo de notes',e)
-                        <Card key={e.id} note={e} removeNote={removeNote} editNote={editNote}/>
+                notes.length > 0
+                ? notes.map(e => (
+                    <Card key={e.id} note={e} removeNote={removeNote} editNote={editNote}/>
                 ))
+                : <EmptyCard />
             }
-            
 		</ul>
     )
 }
